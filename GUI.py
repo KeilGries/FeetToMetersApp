b12a5856bca84f42ef094a0ca5d8c4823aaf7eec
KG <KeilGriesLP@gmail.com>
2023-03-06 17:41:44 -0500

    Minor alignment changes.

--- GUI.py
+++ GUI.py
@@ -9,16 +9,18 @@ input1 = sg.InputText(tooltip='Enter a digit representing feet', key='feet',
 
 label2= sg.Text('Enter inches: ')
 input2 = sg.InputText(tooltip='Enter a digit representing inches', key='inches',
-                      size=18)
+                      size=20)
 
 button1 = sg.Button('Convert')
 button2 = sg.Button('Exit')
 output_label = sg.Text(key='output', text_color='green')
 
+col1 = sg.Column([[label1], [label2], [button1]])
+col2 = sg.Column([[input1], [input2], [button2]])
+
 window = sg.Window('Feet to Meters Convertor', font='Helvetica',
-                                               layout=[[label1, input1],
-                                                       [label2, input2],
-                                                       [button1, button2, output_label]])
+                                               layout=[[col1, col2],
+                                                       [output_label]])
 
 while True:
     event, values = window.read(timeout=200)
